a8c9f468bca96c49e949da155c6fcc475f994593
hmp-anthony <anthonygoddard962@gmail.com>
2022-12-25 17:38:37 +0000

    fleshed test out

--- black_mamba/test_basic.py
+++ black_mamba/test_basic.py
@@ -2,13 +2,18 @@ import black_mamba as bm
 from black_mamba import Var, gradient
 
 
-# Sigmoid
 def sigmoid(x):
 	exp_ = bm.exp()
 	return 1 / (1 + exp_(-x))
 
-x = Var(0.5)
-z = sigmoid(x)
+def exp2(x):
+    exp_ = bm.exp()
+    return exp_(2*x)
 
-def test_gradient_1():
-    assert gradient(z,x) == -0.6065306597126334
+x = Var(0.5)
+z1 = sigmoid(x)
+z2 = exp2(x)
+
+def test_gradient():
+    assert gradient(z1,x) == -0.6065306597126334
+    assert gradient(z2,x) == 5.43656365691809 
