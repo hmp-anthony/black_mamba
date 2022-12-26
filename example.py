652523ae0b124fa2d3a4b0d43c7cea4e8c3cb695
hmp-anthony <anthonygoddard962@gmail.com>
2022-12-26 16:08:15 +0000

    more_examplescd

--- example.py
+++ example.py
@@ -1,2 +1,23 @@
 import black_mamba as bm
 from black_mamba import Var, gradient
+
+
+# Exponentiation
+x = Var(2)
+y = Var(5)
+
+z = x ** y
+
+print(z)			# 32
+print(gradient(z, x))	# 80
+print(gradient(z, y))	# 22.18070977791825
+
+# Sigmoid
+def sigmoid(x):
+	exp_ = bm.exp()
+	return 1 / (1 + exp_(-x))
+
+x = Var(0.5)
+z = sigmoid(x)
+print(z)				# 1.6065306597126334
+print(gradient(z, x))	# -0.6065306597126334
