a0d5c5a8f60d7069a2bac65542d0443b4f06ddf9
hmp-anthony <anthonygoddard962@gmail.com>
2022-12-26 16:18:37 +0000

    proof of graph function

--- example.py
+++ example.py
@@ -17,7 +17,21 @@ def sigmoid(x):
 	exp_ = bm.exp()
 	return 1 / (1 + exp_(-x))
 
+def f(x):
+    return -x;
+def g(x):
+    exp_ = bm.exp()
+    return exp_(x)
+def h(x):
+    return 1 + x;
+def reciprocal(x):
+    return 1 / x
+
 x = Var(0.5)
 z = sigmoid(x)
 print(z)				# 1.6065306597126334
 print(gradient(z, x))	# -0.6065306597126334
+
+z = reciprocal(h(g(f(x))))
+print(z)
+print(gradient(z,x))
